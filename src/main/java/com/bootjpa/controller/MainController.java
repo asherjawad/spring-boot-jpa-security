0944da4046b9ab9d46c41607e3a2b570e43b8f0d
jsiddiqui <jsiddiqui@nisum.com>
2018-05-11 16:31:51 +0500

    password has been changed

--- src/main/java/com/bootjpa/controller/MainController.java
+++ src/main/java/com/bootjpa/controller/MainController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
-	
-	@RequestMapping("/greeting")
+	@GetMapping("/greeting")
     public String index() {
         return "Greetings from Spring Boot!";
     }
